03dd825ac30b797691e576ded9edcde3ebf7dd48
Zakir Hussain Parrey <zakirhussainparrey@gmail.com>
2025-12-18 15:03:42 +0530

    Add examples and notes for comments in Python
    Upload initial files explaining single-line and multi-line comments in Python with examples.

---
+++ Comments/app.py
@@ -0,0 +1,10 @@
+# This is a comment
+# This is a multi-line comment
+# that spans multiple lines
+# to explain the code below
+print("Hello, World!")
+
+# Store the final exam score
+x = 10
+
+x = 9 # Final exam score

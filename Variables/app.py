8232abd7bf9332e59596691082591ab4efa4a862
Zakir Hussain Parrey <zakirhussainparrey@gmail.com>
2025-12-27 16:25:08 +0530

    Add Python variables examples

---
+++ Variables/app.py
@@ -0,0 +1,13 @@
+print("My name is Zakir")
+print("Zakir is learning Python")
+print("Zakir wants to become Python expert")
+
+name = "Steeve"
+print("My name is" , name)
+print(name,"is learning Python")
+print(name,"wants to become Python expert")
+
+email = "datawithzakk.com"
+print("info@",email)
+print("support@",email)
+print("www.",email)

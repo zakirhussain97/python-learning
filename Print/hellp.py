ac813f10a13aa1d6dfaf009f1a7595ca8b504f02
Zakir Hussain Parrey <zakirhussainparrey@gmail.com>
2025-12-27 12:33:13 +0530

    Add print statement examples in Python
    Upload beginner-friendly Python files demonstrating basic print statements, multiple arguments, and simple output formatting.

---
+++ Print/hellp.py
@@ -0,0 +1,8 @@
+print("Hi, this is my first python code") 
+
+print("Your Learning Path: \n \t - Python Basics \n \t - Data Engineering \n \t - AI")
+
+print("""Your Learning Path: 
+\t - Python Basics 
+\t - Data Engineering 
+\t - AI""")
